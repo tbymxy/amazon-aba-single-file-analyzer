92ab6a982c017e0c8594664f2da9e0b931f5ed58
tbymxy <619797446@qq.com>
2026-09-22 09:08:44 +0800

    升级本地数据批量管理至1.7.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'amazon-aba-pwa-v1.6.0';
+const CACHE_NAME = 'amazon-aba-pwa-v1.7.0';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', (event) => {
